3311751d18ddae414b0eaad9f734a98696afef07
Davis <326461396@qq.com>
2021-03-23 15:53:05 +0800

    完成 Tags 组件

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
       <li v-for="tag in dataSource" :key="tag"
@@ -18,7 +18,7 @@
 
   @Component
   export default class Tags extends Vue {
-    @Prop() dataSource: string[] | undefined; // 类型为字符串数组或 undefined
+    @Prop() readonly dataSource: string[] | undefined; // 类型为字符串数组或 undefined; readonly 表示数据不能修改
     selectedTags: string[] = [];
     // 如果标签没有被选中，点击后选中；如果标签已经选中，点击后取消选中
     toggle(tag) {
@@ -27,6 +27,16 @@
         this.selectedTags.splice(index, 1);
       } else {
         this.selectedTags.push(tag);
+      }
+    }
+    create() {
+      const name = window.prompt('请输入标签名'); // 点击"新增标签"，弹出输入对话框，并把输入的内容赋值给 name
+      if (name === '') {
+        window.alert('标签名不能为空');
+      } else {
+        this.$emit('update:dataSource', [...this.dataSource, name]);
+        // ...this.dataSource 表示展开 dataSource 数组
+        // 修改 Prop 的数据，需要在外部组件使用修饰符 .sync
       }
     }
   }
