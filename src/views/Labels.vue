d8483c4d5ec79d1a0206de41ec4fd5595440dd99
Davis <326461396@qq.com>
2021-03-25 22:37:11 +0800

    添加 Labels 组件的 html 和 css

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,15 +1,64 @@
 <template>
   <layout>
-    <p>labels</p>
+    <ul class="tags">
+      <li>
+        <span>衣</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>食</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>住</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>行</span>
+        <Icon name="right"/>
+      </li>
+    </ul>
+    <div class="createTag-wrapper">
+      <button class="createTag">新建标签</button>
+    </div>
   </layout>
 </template>
 
 <script lang="ts">
-export default {
-name: "Labels"
-}
+  export default {
+    name: 'Labels'
+  };
 </script>
 
 <style lang="scss" scoped>
-
+  .tags {
+    background: white;
+    font-size: 16px;
+    padding: 0 16px;
+    li {
+      min-height: 44px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      border-bottom: 1px solid #e6e6e6;
+      .icon {
+        width: 18px;
+        height: 18px;
+        color: #666;
+      }
+    }
+  }
+  .createTag {
+    background: #767676;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    height: 40px;
+    padding: 0 16px;
+    &-wrapper {
+      text-align: center;
+      padding: 16px;
+      margin-top: 44-16px;
+    }
+  }
 </style>
