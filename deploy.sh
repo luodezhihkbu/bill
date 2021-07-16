737189d734f58ad1bc440a60a2de8d206b085bfc
Davis <326461396@qq.com>
2021-07-16 12:50:52 +0800

    修改部署仓库地址

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:luodezhihkbu/morney-1-dist.git master:gh-pages
+git push -f git@github.com:luodezhihkbu/bill-dist.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/morney-1-dist/'
+    ? '/bill-dist/'
     : '/',
   lintOnSave: false,
   chainWebpack: config =>{
